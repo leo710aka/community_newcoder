248796288104338443213c6e0e092f389d38efb6
caifeng <1908454905@qq.com>
2023-09-18 00:03:37 +0800

    帖子相关

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -1,7 +1,9 @@
 package com.nowcoder.community.dao;
 
 import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -12,6 +14,15 @@ public interface DiscussPostMapper {
     List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
 
     int selectDiscussPostRows(@Param("userId") int userId);
+
+//    @Insert({
+//            "insert into discuss_post(user_id,title,content,type,status,createTime,commentCount,score) ",
+//            "values(#{userId},#{title},#{content},#{type},#{status},#{createTime},#{commentCount},#{score})"
+//    })
+//    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertDiscussPost(DiscussPost discussPost);
+
+    DiscussPost selectDiscussPostById(int id);
 
 
 }
--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -2,9 +2,12 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.util.SensitiveFilter;
 import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
+import org.thymeleaf.spring5.processor.SpringErrorClassTagProcessor;
 
 import java.util.List;
 
@@ -14,6 +17,9 @@ public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
@@ -21,5 +27,24 @@ public class DiscussPostService {
     public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
     }
+
+    public int addDiscussPost(DiscussPost post) {
+        if (post == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+        // 转义html
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        // 过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
+    }
+
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
+    }
+
 
 }
