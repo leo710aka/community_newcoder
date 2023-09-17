d10da3b2c0622c00b2ba0f52b9003ca25970c84e
caifeng <1908454905@qq.com>
2023-09-17 12:10:07 +0800

    登录信息【拦截器】

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,35 @@
+package com.nowcoder.community.controller.interceptor;
+
+import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class LoginRequiredInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Method method = handlerMethod.getMethod();
+            // 判断是否是添加了自定义登录拦截的注解
+            LoginRequired loginRequired = method.getAnnotation(LoginRequired.class);
+            // 当前方法需要登录 且 还未登录
+            if (loginRequired != null && hostHolder.getUser() == null) {
+                response.sendRedirect(request.getContextPath() + "/login");
+                return false;
+            }
+        }
+        return true;
+    }
+}
