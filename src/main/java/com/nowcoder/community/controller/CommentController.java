e5f00877df5ba3b6620e95dbf26eb6be6aa356c1
caifeng <1908454905@qq.com>
2023-09-30 23:22:15 +0800

    搜索功能【Elasticsearch】

--- src/main/java/com/nowcoder/community/controller/CommentController.java
+++ src/main/java/com/nowcoder/community/controller/CommentController.java
@@ -3,16 +3,13 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.entity.Comment;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.Event;
-import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.event.EventProducer;
 import com.nowcoder.community.service.CommentService;
 import com.nowcoder.community.service.DiscussPostService;
-import com.nowcoder.community.service.MessageService;
 import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,10 +22,13 @@ public class CommentController implements CommunityConstant {
 
     @Autowired
     private CommentService commentService;
+
     @Autowired
     private HostHolder hostHolder;
+
     @Autowired
     private EventProducer eventProducer;
+
     @Autowired
     private DiscussPostService discussPostService;
 
@@ -51,9 +51,19 @@ public class CommentController implements CommunityConstant {
             event.setEntityUserId(target.getUserId());
         } else if (comment.getEntityType() == ENTITY_TYPE_COMMENT) {
             Comment target = commentService.findCommentById(comment.getEntityId());
-            event.setEntityId(target.getUserId());
+            event.setEntityUserId(target.getUserId());
         }
         eventProducer.fireEvent(event);
+
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            // 触发发帖事件
+            event = new Event()
+                    .setTopic(TOPIC_PUBLISH)
+                    .setUserId(comment.getUserId())
+                    .setEntityType(ENTITY_TYPE_POST)
+                    .setEntityId(discussPostId);
+            eventProducer.fireEvent(event);
+        }
 
         return "redirect:/discuss/detail/" + discussPostId;
     }
--- src/main/java/com/nowcoder/community/event/EventConsumer.java
+++ src/main/java/com/nowcoder/community/event/EventConsumer.java
@@ -1,18 +1,19 @@
 package com.nowcoder.community.event;
 
 import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.Event;
 import com.nowcoder.community.entity.Message;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.ElasticsearchService;
 import com.nowcoder.community.service.MessageService;
 import com.nowcoder.community.util.CommunityConstant;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.hibernate.validator.spi.scripting.ScriptEvaluatorNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import org.thymeleaf.context.IContext;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -26,23 +27,32 @@ public class EventConsumer implements CommunityConstant {
     @Autowired
     private MessageService messageService;
 
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private ElasticsearchService elasticsearchService;
+
     @KafkaListener(topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
     public void handleCommentMessage(ConsumerRecord record) {
         if (record == null || record.value() == null) {
-            logger.error("消息内容空！");
-            return;
-        }
-        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
-        if (event == null) {
-            logger.error("消息格式错！");
+            logger.error("消息的内容为空!");
             return;
         }
 
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误!");
+            return;
+        }
+
+        // 发送站内通知
         Message message = new Message();
         message.setFromId(SYSTEM_USER_ID);
         message.setToId(event.getEntityUserId());
         message.setConversationId(event.getTopic());
         message.setCreateTime(new Date());
+
         Map<String, Object> content = new HashMap<>();
         content.put("userId", event.getUserId());
         content.put("entityType", event.getEntityType());
@@ -53,10 +63,27 @@ public class EventConsumer implements CommunityConstant {
                 content.put(entry.getKey(), entry.getValue());
             }
         }
+
         message.setContent(JSONObject.toJSONString(content));
         messageService.addMessage(message);
+    }
 
+    // 消费发帖事件
+    @KafkaListener(topics = {TOPIC_PUBLISH})
+    public void handlePublishMessage(ConsumerRecord record) {
+        if (record == null || record.value() == null) {
+            logger.error("消息的内容为空!");
+            return;
+        }
 
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误!");
+            return;
+        }
+
+        DiscussPost post = discussPostService.findDiscussPostById(event.getEntityId());
+        elasticsearchService.saveDiscussPost(post);
     }
 
 }
